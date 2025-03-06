c0b860781ff780d6d87d624a0d84527e4f39af16
3222h <394954a+++@gmail.com>
2025-03-06 12:12:18 +0530

    Update tor.sh

--- tor.sh
+++ tor.sh
@@ -5,6 +5,15 @@ then
     sudo apt update
     sudo apt install -y tor
 fi
+TORRC_FILE="/etc/tor/torrc"
 
+sudo bash -c "cat > $TORRC_FILE" <<EOL
+SocksPort 9050
+ControlPort 9051
+CookieAuthentication 1
+ExitNodes {us}
+StrictNodes 1
+EOL
+sleep 2
 sudo service tor start
 sudo service tor status

33c82d903fdfe7cbf496b68950dfec9465473364
3222h <394954a+++@gmail.com>
2025-03-06 11:03:39 +0530

    Update tor.sh

--- tor.sh
+++ tor.sh
@@ -6,9 +6,7 @@ then
     sudo apt install -y tor
 fi
 
-TORRC_FILE="/etc/tor/torrc"
-sudo sed -i '/^ExitNodes/d' "$TORRC_FILE"
-sudo sed -i '/^StrictNodes/d' "$TORRC_FILE"
+
 
 
 sudo service tor start

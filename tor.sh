b256e2afd7dd86c319806c326db9bc4f2ebb8c9b
3222h <394954a+++@gmail.com>
2025-03-06 13:20:39 +0530

    Update tor.sh

--- tor.sh
+++ tor.sh
@@ -5,15 +5,19 @@ then
     sudo apt update
     sudo apt install -y tor
 fi
+
+echo "======================="
+echo "choose region (USE CAPITAL LATTER ONLY)."
+echo "======================="
+echo "US,AU,SG,FR,DE,LU,JP,CA,BE,GB,NL,IS,HK,BR,IT,PL,RO"
+read -p "choose region: " CRP
+
 TORRC_FILE="/etc/tor/torrc"
-
-#US,AU,SG,FR,DE,LU,JP,CA,BE,GB,NL,IS,HK,BR,IT,PL,RO
-
 sudo bash -c "cat > $TORRC_FILE" <<EOL
 SocksPort 9050
 ControlPort 9051
 CookieAuthentication 1
-ExitNodes {US}
+ExitNodes {$CRP}
 StrictNodes 1
 EOL
 sleep 1

3ecd0c659f1530e67797a007a14d992cd3209d0e
3222h <394954a+++@gmail.com>
2025-03-12 18:17:45 +0530

    Update tor.sh

--- tor.sh
+++ tor.sh
@@ -9,17 +9,27 @@ then
 fi
 
 clear
-echo "======================="
-echo "Choose region (USE CAPITAL LETTERS ONLY)."
-echo "======================="
-echo "US, AU, SG, FR, DE, LU, JP, CA, BE, GB, NL, IS, HK, BR, IT, PL, RO"
-
-
-read -p "Choose region: " CRP
-CRP=$(echo "$CRP" | tr '[:lower:]' '[:upper:]')
-
-
+TORVPN_FILE="TORVPN"
 TORRC_FILE="/etc/tor/torrc"
+
+# Check if TORVPN file exists and is not empty
+if [ -s "$TORVPN_FILE" ]; then
+    CRP=$(cat "$TORVPN_FILE")
+    echo "Using saved region: $CRP"
+else
+    echo "======================="
+    echo "Choose region (USE CAPITAL LETTERS ONLY)."
+    echo "======================="
+    echo "US, AU, SG, FR, DE, LU, JP, CA, BE, GB, NL, IS, HK, BR, IT, PL, RO"
+
+    read -p "Choose region: " CRP
+    CRP=$(echo "$CRP" | tr '[:lower:]' '[:upper:]')
+
+    # Save the region to the TORVPN file
+    echo "$CRP" > "$TORVPN_FILE"
+fi
+
+# Create or update the torrc file
 sudo bash -c "cat > $TORRC_FILE" <<EOL
 SocksPort 9050
 ControlPort 9051
@@ -27,6 +37,8 @@ CookieAuthentication 1
 ExitNodes {$CRP}
 StrictNodes 1
 EOL
+
+echo "Tor configuration updated with region: $CRP"
 clear
 
 sleep 1
